d6a9f7747fbb254c8c0841dafa89dded4464b35f
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-05-31 20:14:51 +0700

    OK

--- Convolution.py
+++ Convolution.py
@@ -3,11 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv.imread('CV.png', 0)
-
-kernel = np.array([[0, -1, 0],
-                   [-1, 5, -1],
-                   [0, -1, 0]])
-
+kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
 sharpened = cv.filter2D(img, -1, kernel)
 
 plt.subplot(1, 2, 1), plt.imshow(img, cmap='gray'), plt.title('Original')
