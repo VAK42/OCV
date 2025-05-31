7b779e6dd5e4843bc86dc068ac432d156b03a90d
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-05-31 17:33:32 +0700

    VAK

--- Transformations.py
+++ Transformations.py
@@ -6,19 +6,33 @@ img = cv.imread('CV.png', 0)  # Read Input Image In Grayscale Mode
 # cv.IMREAD_COLOR :	1
 # cv.IMREAD_GRAYSCALE :	0
 # cv.IMREAD_UNCHANGED : -1
-c = 255 / np.log(1 + np.max(img))
-log_transformed = c * np.log(1 + img.astype(np.float32))
-log_transformed = np.uint8(log_transformed)
+
+c = 255 / np.log(1 + np.max(img))  # 255 / log(1 + max(r))  0 <= r <= 255
+logTransformed = c * np.log(1 + img.astype(np.float32))  # s = T(r) = c * log(1 + r)
+logTransformed = np.uint8(logTransformed)
+# Brighten Images With Low Intensity
+# Narrow - Dark Pixel Ranges -> Wider - Brighter
+# Compress Bright Pixels Into A Smaller Range
+
 gamma = 2.0
-gamma_corrected = np.array(255 * (img / 255) ** gamma, dtype='uint8')
-negative = 255 - img
+gammaCorrected = np.array(255 * (img / 255) ** gamma, dtype='uint8')
+# s = T(r) = c * r ** γ
+# γ < 1: Brightens The Image
+# γ > 1: Darkens The Image
+# Adjust Image Brightness & Contrast
+
+negative = 255 - img  # s = T(r) = 255 - r
+# Enhance White & Gray Details In Images With Predominantly Dark Regions
+
 def contrastStretch(img):
     a, b = np.min(img), np.max(img)
     return ((img - a) * 255 / (b - a)).astype('uint8')
 stretched = contrastStretch(img)
+# Enhance Contrast In Low-Contrast Images
+
 cv.imshow("Original", img)
-cv.imshow("Log Transformed", log_transformed)
-cv.imshow("Gamma Corrected", gamma_corrected)
+cv.imshow("Log Transformed", logTransformed)
+cv.imshow("Gamma Corrected", gammaCorrected)
 cv.imshow("Negative", negative)
 cv.imshow("Contrast Stretched", stretched)
 cv.waitKey(0)
