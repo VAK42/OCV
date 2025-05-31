0e1e6c456b10c2638dd15197f55a4f1cfe8e3196
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-05-31 23:04:11 +0700

    OK

---
+++ HoughCircle.py
@@ -0,0 +1,25 @@
+import numpy as np
+import cv2 as cv
+from matplotlib import pyplot as plt
+
+img = cv.imread('CV.png', cv.IMREAD_GRAYSCALE)
+assert img is not None, "File Could Not Be Read - Check With os.path.exists()"
+
+img = cv.medianBlur(img, 5)
+
+cimg = cv.cvtColor(img, cv.COLOR_GRAY2BGR)
+
+circles = cv.HoughCircles(img, cv.HOUGH_GRADIENT, 1, 20, param1=50, param2=30, minRadius=0, maxRadius=0)
+
+circles = np.uint16(np.around(circles))
+
+for i in circles[0, :]:
+    cv.circle(cimg, (i[0], i[1]), i[2], (0, 255, 0), 2)
+    cv.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
+
+cimg = cv.cvtColor(cimg, cv.COLOR_BGR2RGB)
+
+plt.imshow(cimg)
+plt.title('Detected Circles')
+plt.axis('off')
+plt.show()
