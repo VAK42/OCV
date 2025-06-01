43b324cb40a446fcb5259adf71a4ce8d7acfef3a
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-02 00:17:24 +0700

    OK

--- Gradient.py
+++ Gradient.py
@@ -6,13 +6,15 @@ img = cv.imread('CV.png', cv.IMREAD_GRAYSCALE)
 assert img is not None, "File Could Not Be Read - Check With os.path.exists()"
 
 laplacian = cv.Laplacian(img, cv.CV_64F)
+# Tính Đạo Hàm Bậc Hai Của Ảnh Để Phát Hiện Các Vùng Có Biên Rõ
 sobelx = cv.Sobel(img, cv.CV_64F, 1, 0, ksize=5)
+# Tính Đạo Hàm Bậc Nhất Theo Trục X
 sobely = cv.Sobel(img, cv.CV_64F, 0, 1, ksize=5)
-
-sobelx8u = cv.Sobel(img, cv.CV_8U, 1, 0, ksize=5)
-sobelx64f = cv.Sobel(img, cv.CV_64F, 1, 0, ksize=5)
-abs_sobel64f = np.absolute(sobelx64f)
+# Tính Đạo Hàm Bậc Nhất Theo Trục Y
+abs_sobel64f = np.absolute(sobelx)
+# Lấy Giá Trị Tuyệt Đối Của Gradient Để Dễ Biểu Diễn Trên Ảnh
 sobel_8u = np.uint8(abs_sobel64f)
+# Ép Kết Quả Về Kiểu uint8 (0–255) Để Có Thể Hiển Thị Trên Ảnh
 
 plt.subplot(2,3,1), plt.imshow(img, cmap='gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
