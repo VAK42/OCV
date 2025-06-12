c26dd7528287e28c3f51111754c6df114f731045
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-13 00:49:51 +0700

    OK

--- HoughLine.py
+++ HoughLine.py
@@ -7,19 +7,10 @@ assert img is not None, "File Could Not Be Read - Check With os.path.exists()"
 
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 edges = cv.Canny(gray, 50, 150, apertureSize=3)
-# 50: Ngưỡng Dưới Trong Canny
-# 150: Ngưỡng Trên Trong Canny
-# apertureSize=3: Kích Thước Kernel Sobel (3×3)
-
 lines = cv.HoughLines(edges, 1, np.pi / 180, 200)
-# 1: Độ Phân Giải Khoảng Cách (Pixel)
-# np.pi / 180: Độ Phân Giải Góc (π/180 Radian)
-# 200: Ngưỡng Số Bỏ Phiếu Tối Thiểu Để Xác Nhận Dòng
 
 for line in lines:
     rho, theta = line[0]
-    # rho: Khoảng Cách Từ Gốc Tọa Độ (0,0) Đến Đường Thẳng
-    # theta: Góc Giữa Trục X & Pháp Tuyến Của Đường Thẳng
     a = np.cos(theta)
     b = np.sin(theta)
     x0 = a * rho
@@ -32,11 +23,6 @@ for line in lines:
 
 img2 = img.copy()
 linesP = cv.HoughLinesP(edges, 1, np.pi / 180, 100, minLineLength=100, maxLineGap=10)
-# 1: Độ Phân Giải Khoảng Cách
-# np.pi / 180: Độ Phân Giải Góc
-# 100: Ngưỡng Số Bỏ Phiếu Tối Thiểu Để Xác Nhận Dòng
-# minLineLength=100: Chiều Dài Dòng Tối Thiểu
-# maxLineGap=10: Khoảng Cách Tối Đa Giữa Các Điểm Để Nối Thành Dòng
 
 for line in linesP:
     x1, y1, x2, y2 = line[0]
@@ -49,10 +35,8 @@ plt.subplot(121)
 plt.imshow(img)
 plt.title('Hough Lines Standard')
 plt.axis('off')
-
 plt.subplot(122)
 plt.imshow(img2)
 plt.title('Hough Lines Probabilistic')
 plt.axis('off')
-
 plt.show()
