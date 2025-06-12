054de47f750db624e7d9c54814e4e12ade6a7ba4
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-13 00:44:15 +0700

    OK

--- Canny.py
+++ Canny.py
@@ -43,8 +43,6 @@ edges = cv.Canny(img, 100, 200)
 
 plt.subplot(121), plt.imshow(img, cmap='gray')
 plt.title('Original Image'), plt.xticks([]), plt.yticks([])
-
 plt.subplot(122), plt.imshow(edges, cmap='gray')
 plt.title('Edge Image'), plt.xticks([]), plt.yticks([])
-
 plt.show()
