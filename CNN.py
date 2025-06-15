d54761a3928a199e6b3669065312769af167d8ee
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-16 01:30:20 +0700

    CNN

--- CNN.py
+++ CNN.py
@@ -1,31 +1,27 @@
 import matplotlib.pyplot as plt
 import tensorflow as tf
-from keras import layers, models
+from tensorflow.keras import layers, models
 
 (x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+x_train = tf.image.rgb_to_grayscale(tf.image.resize(x_train, [64, 64])) / 255.0
+x_test = tf.image.rgb_to_grayscale(tf.image.resize(x_test, [64, 64])) / 255.0
 
-x_train = x_train.astype("float32") / 255.0
-x_test = x_test.astype("float32") / 255.0
-
+input_shape = (64, 64, 1)
 model = models.Sequential([
-    layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)),
+    layers.Conv2D(10, (3, 3), activation='relu', padding='valid', input_shape=input_shape),
+    layers.Conv2D(10, (3, 3), activation='relu', padding='valid'),
     layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
-    layers.MaxPooling2D((2, 2)),
-    layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
+    layers.Conv2D(10, (3, 3), activation='relu', padding='valid'),
+    layers.Conv2D(10, (3, 3), activation='relu', padding='valid'),
     layers.MaxPooling2D((2, 2)),
     layers.Flatten(),
-    layers.Dense(128, activation='relu'),
-    layers.Dropout(0.5),
     layers.Dense(10, activation='softmax')
 ])
 
 model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
-
 model.summary()
 
 history = model.fit(x_train, y_train, epochs=2, validation_data=(x_test, y_test), batch_size=64)
-
 test_loss, test_acc = model.evaluate(x_test, y_test)
 print(f"\nTest Accuracy: {test_acc:.4f}")
 
