030ae4bfc9cf284fe9c3172dd29d7ceff0e21470
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-15 22:02:44 +0700

    CNN

--- CNN.py
+++ CNN.py
@@ -1,2 +1,48 @@
+import matplotlib.pyplot as plt
+import tensorflow as tf
+from keras import layers, models
+
+(x_train, y_train), (x_test, y_test) = tf.keras.datasets.cifar10.load_data()
+
+x_train = x_train.astype("float32") / 255.0
+x_test = x_test.astype("float32") / 255.0
+
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', padding='same', input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu', padding='same'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(128, (3, 3), activation='relu', padding='same'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Flatten(),
+    layers.Dense(128, activation='relu'),
+    layers.Dropout(0.5),
+    layers.Dense(10, activation='softmax')
+])
+
+model.compile(optimizer='adam', loss='sparse_categorical_crossentropy', metrics=['accuracy'])
+
+model.summary()
+
+history = model.fit(x_train, y_train, epochs=2, validation_data=(x_test, y_test), batch_size=64)
+
+test_loss, test_acc = model.evaluate(x_test, y_test)
+print(f"\nTest Accuracy: {test_acc:.4f}")
+
+plt.figure(figsize=(12, 5))
+plt.subplot(1, 2, 1)
+plt.plot(history.history['accuracy'], label='Train Acc')
+plt.plot(history.history['val_accuracy'], label='Val Acc')
+plt.title('Accuracy')
+plt.legend()
+
+plt.subplot(1, 2, 2)
+plt.plot(history.history['loss'], label='Train Loss')
+plt.plot(history.history['val_loss'], label='Val Loss')
+plt.title('Loss')
+plt.legend()
+plt.show()
+
 # https://poloclub.github.io/cnn-explainer
 # https://www.tensorflow.org/tutorials/images/cnn
+# https://colab.research.google.com/drive/1HDyOt73q8nKc1BO1Ol9JmJGo0GDZ-BBY
