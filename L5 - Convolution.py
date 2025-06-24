c37596f58fd60df9c52ce7cc6569a0a6b2638416
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-24 22:15:34 +0700

    .

--- L5 - Convolution.py
+++ L5 - Convolution.py
@@ -3,8 +3,14 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 img = cv.imread('CV.png', 0)
+# 0: Grayscale
 kernel = np.array([[0, -1, 0], [-1, 5, -1], [0, -1, 0]])
+# Define A Sharpening Kernel - 3x3 Matrix
+# Enhance The Center Pixel & Subtract The Neighboring Ones
+# Sharpen The Image By Emphasizing Edges & Details
 sharpened = cv.filter2D(img, -1, kernel)
+# Apply The Sharpening Kernel To The Image Using 2D Convolution
+# -1: The Output Image Will Have The Same Depth As The Input Image
 
 plt.subplot(1, 2, 1), plt.imshow(img, cmap='gray'), plt.title('Original')
 plt.subplot(1, 2, 2), plt.imshow(sharpened, cmap='gray'), plt.title('Sharpened')
