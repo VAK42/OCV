7163eb05966f19c0916a0c8c207ad29919f78c8c
ヴ・アイン・キエト <vutrandangkhoa7@gmail.com>
2025-06-25 11:11:39 +0700

    .

--- L7 - Gradient.py
+++ L7 - Gradient.py
@@ -6,10 +6,28 @@ img = cv.imread('CV.png', cv.IMREAD_GRAYSCALE)
 assert img is not None, "File Could Not Be Read - Check With os.path.exists()"
 
 laplacian = cv.Laplacian(img, cv.CV_64F)
+# Laplacian - Detect Edges In All Directions
+# cv.CV_64F Specifies The Desired Depth Of The Output Image To Prevent Data Loss From Negative Gradients
+
 sobelx = cv.Sobel(img, cv.CV_64F, 1, 0, ksize=5)
+# 1: Order Of Derivative In X-Direction
+# 0: Order Of Derivative In Y-Direction
+# ksize=5: Size Of Sobel Kernel (Larger Kernel = Smoother Result)
+
 sobely = cv.Sobel(img, cv.CV_64F, 0, 1, ksize=5)
+# 1: Order Of Derivative In X-Direction
+# 0: Order Of Derivative In Y-Direction
+# ksize=5: Size Of Sobel Kernel (Larger Kernel = Smoother Result)
+
 abs_sobel64f = np.absolute(sobelx)
+# Take Absolute Value Of The X-Direction Sobel Result
+# Necessary Because Gradient Values Can Be Negative
+# -> Avoid Clipping Useful Data When Converting To 8-Bit
+# -> Suitable For Visualization
+
 sobel_8u = np.uint8(abs_sobel64f)
+# Convert 64-Bit Float To 8-Bit Unsigned Integer
+# Useful For Displaying Or Saving The Image As Most Image Formats Expect 8-Bit Values
 
 plt.subplot(2,3,1), plt.imshow(img, cmap='gray')
 plt.title('Original'), plt.xticks([]), plt.yticks([])
